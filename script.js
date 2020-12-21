88cc7240beaef36cdff37d3208e162e95f757692
GoldenManBel <74560637+GoldenManBel@users.noreply.github.com>
2020-12-21 19:13:53 +0300

    fix: portfolio

--- script.js
+++ script.js
@@ -38,161 +38,51 @@ bottom2.addEventListener('click', (event) => {
      slider.classList.toggle('slider-action');  
 });
 
-
-/* portfolio */
-const portfolio = document.querySelectorAll('.portfolio_image>div img');
-let arr = [portfolio[0], portfolio[1], portfolio[2], portfolio[3], portfolio[4], portfolio[5], portfolio[6], 
-portfolio[7], portfolio[8], portfolio[9], portfolio[10], portfolio[11]];
+/* Portfolio */
 
 const buttonOne = document.querySelector('.button__portfolio-1');
 const buttonTwo = document.querySelector('.button__portfolio-2');
 const buttonThree = document.querySelector('.button__portfolio-3');
 const buttonFour = document.querySelector('.button__portfolio-4');
+const menu = document.querySelector('.portfolio_image');
 
 buttonOne.addEventListener('click', (event) => {
-    function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
-      }
-      shuffle(arr);
-      buttonOne.classList.toggle('active-2');
-      buttonTwo.classList.remove('active-2');
-      buttonThree.classList.remove('active-2');
-      buttonFour.classList.remove('active-2');
+        for (let i = menu.children.length; i >= 0; i--) {
+        menu.appendChild(menu.children[Math.random() * i | 0]);
 
-let arr_src = arr[1].getAttribute('src');
-let arr_src_1 = arr[2].getAttribute('src');
-let arr_src_2 = arr[3].getAttribute('src');
-let arr_src_3 = arr[4].getAttribute('src');
-let arr_src_4 = arr[5].getAttribute('src');
-let arr_src_5 = arr[6].getAttribute('src');
-let arr_src_6 = arr[7].getAttribute('src');
-let arr_src_7 = arr[8].getAttribute('src');
-let arr_src_8 = arr[9].getAttribute('src');
-let arr_src_9 = arr[10].getAttribute('src');
-let arr_src_10 = arr[11].getAttribute('src');
-let arr_src_11 = arr[0].getAttribute('src');
-
-arr[0].setAttribute('src', arr_src);
-arr[1].setAttribute('src', arr_src_1);
-arr[2].setAttribute('src', arr_src_2);
-arr[3].setAttribute('src', arr_src_3);
-arr[4].setAttribute('src', arr_src_4);
-arr[5].setAttribute('src', arr_src_5);
-arr[6].setAttribute('src', arr_src_6);
-arr[7].setAttribute('src', arr_src_7);
-arr[8].setAttribute('src', arr_src_8);
-arr[9].setAttribute('src', arr_src_9);
-arr[10].setAttribute('src', arr_src_10);
-arr[11].setAttribute('src', arr_src_11);
-});
-
+            buttonOne.classList.toggle('active-2');
+            buttonTwo.classList.remove('active-2');
+            buttonThree.classList.remove('active-2');
+            buttonFour.classList.remove('active-2');
+}
+})
 buttonTwo.addEventListener('click', (event) => {
-    function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
-      }
-      shuffle(arr);
-      buttonTwo.classList.toggle('active-2');
-      buttonOne.classList.remove('active-2');
-      buttonThree.classList.remove('active-2');
-      buttonFour.classList.remove('active-2');
+    for (let i = menu.children.length; i >= 0; i--) {
+    menu.appendChild(menu.children[Math.random() * i | 0]);
 
-let arr_src = arr[1].getAttribute('src');
-let arr_src_1 = arr[2].getAttribute('src');
-let arr_src_2 = arr[3].getAttribute('src');
-let arr_src_3 = arr[4].getAttribute('src');
-let arr_src_4 = arr[5].getAttribute('src');
-let arr_src_5 = arr[6].getAttribute('src');
-let arr_src_6 = arr[7].getAttribute('src');
-let arr_src_7 = arr[8].getAttribute('src');
-let arr_src_8 = arr[9].getAttribute('src');
-let arr_src_9 = arr[10].getAttribute('src');
-let arr_src_10 = arr[11].getAttribute('src');
-let arr_src_11 = arr[0].getAttribute('src');
-
-arr[0].setAttribute('src', arr_src);
-arr[1].setAttribute('src', arr_src_1);
-arr[2].setAttribute('src', arr_src_2);
-arr[3].setAttribute('src', arr_src_3);
-arr[4].setAttribute('src', arr_src_4);
-arr[5].setAttribute('src', arr_src_5);
-arr[6].setAttribute('src', arr_src_6);
-arr[7].setAttribute('src', arr_src_7);
-arr[8].setAttribute('src', arr_src_8);
-arr[9].setAttribute('src', arr_src_9);
-arr[10].setAttribute('src', arr_src_10);
-arr[11].setAttribute('src', arr_src_11);
-});
-
+             buttonOne.classList.remove('active-2');
+             buttonTwo.classList.toggle('active-2');
+             buttonThree.classList.remove('active-2');
+             buttonFour.classList.remove('active-2');
+}
+})
 buttonThree.addEventListener('click', (event) => {
-    function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
-      }
-      shuffle(arr);
-      buttonThree.classList.toggle('active-2');
-      buttonOne.classList.remove('active-2');
-      buttonTwo.classList.remove('active-2');
-      buttonFour.classList.remove('active-2');
+    for (let i = menu.children.length; i >= 0; i--) {
+    menu.appendChild(menu.children[Math.random() * i | 0]);
 
-let arr_src = arr[1].getAttribute('src');
-let arr_src_1 = arr[2].getAttribute('src');
-let arr_src_2 = arr[3].getAttribute('src');
-let arr_src_3 = arr[4].getAttribute('src');
-let arr_src_4 = arr[5].getAttribute('src');
-let arr_src_5 = arr[6].getAttribute('src');
-let arr_src_6 = arr[7].getAttribute('src');
-let arr_src_7 = arr[8].getAttribute('src');
-let arr_src_8 = arr[9].getAttribute('src');
-let arr_src_9 = arr[10].getAttribute('src');
-let arr_src_10 = arr[11].getAttribute('src');
-let arr_src_11 = arr[0].getAttribute('src');
-
-arr[0].setAttribute('src', arr_src);
-arr[1].setAttribute('src', arr_src_1);
-arr[2].setAttribute('src', arr_src_2);
-arr[3].setAttribute('src', arr_src_3);
-arr[4].setAttribute('src', arr_src_4);
-arr[5].setAttribute('src', arr_src_5);
-arr[6].setAttribute('src', arr_src_6);
-arr[7].setAttribute('src', arr_src_7);
-arr[8].setAttribute('src', arr_src_8);
-arr[9].setAttribute('src', arr_src_9);
-arr[10].setAttribute('src', arr_src_10);
-arr[11].setAttribute('src', arr_src_11);
-});
-
+            buttonOne.classList.remove('active-2');
+            buttonTwo.classList.remove('active-2');
+            buttonThree.classList.toggle('active-2');
+            buttonFour.classList.remove('active-2');
+}
+})
 buttonFour.addEventListener('click', (event) => {
-    function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
-      }
-      shuffle(arr);
-      buttonFour.classList.toggle('active-2');
-      buttonOne.classList.remove('active-2');
-      buttonTwo.classList.remove('active-2');
-      buttonThree.classList.remove('active-2');
-      
-let arr_src = arr[1].getAttribute('src');
-let arr_src_1 = arr[2].getAttribute('src');
-let arr_src_2 = arr[3].getAttribute('src');
-let arr_src_3 = arr[4].getAttribute('src');
-let arr_src_4 = arr[5].getAttribute('src');
-let arr_src_5 = arr[6].getAttribute('src');
-let arr_src_6 = arr[7].getAttribute('src');
-let arr_src_7 = arr[8].getAttribute('src');
-let arr_src_8 = arr[9].getAttribute('src');
-let arr_src_9 = arr[10].getAttribute('src');
-let arr_src_10 = arr[11].getAttribute('src');
-let arr_src_11 = arr[0].getAttribute('src');
+    for (let i = menu.children.length; i >= 0; i--) {
+    menu.appendChild(menu.children[Math.random() * i | 0]);
 
-arr[0].setAttribute('src', arr_src);
-arr[1].setAttribute('src', arr_src_1);
-arr[2].setAttribute('src', arr_src_2);
-arr[3].setAttribute('src', arr_src_3);
-arr[4].setAttribute('src', arr_src_4);
-arr[5].setAttribute('src', arr_src_5);
-arr[6].setAttribute('src', arr_src_6);
-arr[7].setAttribute('src', arr_src_7);
-arr[8].setAttribute('src', arr_src_8);
-arr[9].setAttribute('src', arr_src_9);
-arr[10].setAttribute('src', arr_src_10);
-arr[11].setAttribute('src', arr_src_11);
-});
+            buttonOne.classList.remove('active-2');
+            buttonTwo.classList.remove('active-2');
+            buttonThree.classList.remove('active-2');
+            buttonFour.classList.toggle('active-2');
+}
+})
