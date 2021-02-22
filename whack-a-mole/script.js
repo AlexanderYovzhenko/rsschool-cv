7e2f06bfd1d6ba3848ca3affdd004eaa4b8d6a98
GoldenManBel <74560637+GoldenManBel@users.noreply.github.com>
2021-02-22 13:19:44 +0300

    Fixed code

--- whack-a-mole/script.js
+++ whack-a-mole/script.js
@@ -7,52 +7,51 @@ const recordText = document.querySelector('.record-text');
 const seeTimeLevel = document.querySelector('.time');
 
 let lastHole;
-let timeUp = false;
+let timeUp = true;
 let score = 0;
 let scoreGlobal;
 let minTime = 0;
 let maxTime = 0;
-let startGameActive = false;
-let level;
+let level = 0;
 let timerSecond;
 let timeLevel;
+let moleTime;
 
 function levelTimeCalc() { 
    timerSecond = setInterval(() => {
     if(+seeTimeLevel.innerText > 0) {
-      seeTimeLevel.innerText -= 1  
+      seeTimeLevel.innerText -= 1;  
     }
   }, 1000); 
 }
 
 function levelSelection(e) {
+  clearInterval(timerSecond);
+  clearTimeout(timeLevel);
   level = +e.target.dataset.level;
   localStorageRecord();
   levelSelectionLook(e);
   seeTimeLevel.innerText = `choose level`;
-  clearInterval(timerSecond);
-  clearTimeout(timeLevel);
   switch(level) {
     case 1:
-      minTime = 700;
-      maxTime = 800;
+      minTime = 900;
+      maxTime = 1000;
       break; 
    
     case 2: 
-      minTime = 600;
-      maxTime = 700; 
+      minTime = 800;
+      maxTime = 900; 
       break;    
    
     case 3: 
-      minTime = 500;
-      maxTime = 600;
+      minTime = 700;
+      maxTime = 800;
       break; 
   }  
 }
 
 function levelSelectionLook(e) {
     timeUp = true;
-    startGameActive = false;
     e.target.classList.add('active-button');
     for(let i of levels) {
       if(level === +i.dataset.level) continue;
@@ -73,28 +72,33 @@ function randomHole(holes) {
   lastHole = hole;
   return hole;
 }
-   
+let hole = randomHole(holes); 
 function peep() {
   const time = randomTime(minTime, maxTime);
-  const hole = randomHole(holes);
+  hole = randomHole(holes); 
   hole.classList.add('up');
-  setTimeout(() => {
+  moleTime = setTimeout(() => {
     hole.classList.remove('up');
     if (!timeUp) peep();
   }, time);
 }
  
 function startGame() {
+  if(level !== 0) { 
+  clearTimeout(moleTime);
+  clearTimeout(timeLevel);
+  hole.classList.remove('up');  
   clearInterval(timerSecond);
   seeTimeLevel.innerText = 20;
-  levelTimeCalc();
-  if(startGameActive === true) return;  
-  startGameActive = true;  
+  levelTimeCalc();  
   scoreBoard.textContent = 0;
   timeUp = false;
   score = 0;
+  timeLevel = setTimeout(() => {timeUp = true, seeTimeLevel.innerText = 'Game over!'}, 20000);
   peep();
-  timeLevel = setTimeout(() => {timeUp = true, startGameActive = false, seeTimeLevel.innerText = 'Game over!'}, 20000);
+} else {
+  seeTimeLevel.innerText += '!';  
+  }
 }
 
 function bonk(e) {
